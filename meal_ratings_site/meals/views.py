27d04c7fa088009449f06b5023ad08c48c353615
AkinoJoey <gakiya213@gmail.com>
2023-08-13 18:18:21 +0900

    index()にcontextを追加

--- meal_ratings_site/meals/views.py
+++ meal_ratings_site/meals/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Meal, MealRating
+from django.db.models import Avg
+
 
 
 def index(request):
-    return HttpResponse("Hello, world")
+    top3_list = Meal.objects.annotate(avg_rating=Avg('mealrating__rating')).order_by('-avg_rating').values('name', 'imageUrl')[:3]
+    context = {
+        "top3_list": top3_list
+    }
+    return render(request, "meals/index.html", context)
