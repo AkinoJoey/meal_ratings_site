c594082f2d5f42eb04224b3981a1aa1c798af442
AkinoJoey <gakiya213@gmail.com>
2023-08-16 15:25:09 +0900

    imageUrlを変更するための関数を追加

--- meal_ratings_site/meals/models.py
+++ meal_ratings_site/meals/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.db.models import Avg
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 import datetime
+import os
+
 
 class Meal(models.Model):
     class MealTime(models.IntegerChoices):
@@ -10,7 +15,7 @@ class Meal(models.Model):
         
     name = models.CharField(max_length=255)
     description = models.TextField()
-    imageUrl = models.ImageField(upload_to='meals/')
+    imageUrl = models.ImageField(upload_to='images/')
     countryOfOrigin = models.CharField(max_length=255)
     typicalMealTime = models.IntegerField(choices=MealTime.choices)
     dateAdded = models.DateTimeField(default=datetime.datetime.now())
@@ -23,8 +28,26 @@ class Meal(models.Model):
     
     def numberOfVotes(self):
         return self.mealrating_set.count()
-
+    
 class MealRating(models.Model):
     meal = models.ForeignKey(Meal,on_delete=models.CASCADE)
     rating = models.FloatField()
     dateOfRating = models.DateTimeField(default=datetime.datetime.now())
+
+# インスタンスを保存してpkが作成された後に、imageUrlをpkにupdateする
+@receiver(post_save, sender=Meal)
+def set_meal_image_url(sender, instance, **kwargs):
+    Meal.objects.filter(pk=instance.pk).update(imageUrl=f'images/{instance.pk}.jpg')
+
+# インスタンスを保存して、pkが作成された後に、uploadされた画像の名前を変更する
+@receiver(post_save, sender=Meal)
+def rename_meal_image(sender, instance, **kwargs):
+    if instance.imageUrl:
+        old_path = instance.imageUrl.path
+        new_filename = f'{instance.pk}.jpg'
+        new_path = os.path.join(settings.MEDIA_ROOT, 'images', new_filename)
+        
+        if old_path != new_path:
+            os.rename(old_path, new_path)
+            instance.imageUrl.name = f'images/{new_filename}'
+            instance.save(update_fields=['imageUrl'])
